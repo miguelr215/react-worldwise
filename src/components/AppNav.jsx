89b1706ad0ced32ec08ac061cb188418387af83a
miguelr215 <drexelramos@gmail.com>
2025-04-17 10:50:46 -0400

    components added

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,8 @@
+import { Link } from 'react-router-dom';
+import styles from './AppNav.module.css';
+
+function AppNav() {
+	return <nav className={styles.appNav}>App navigation</nav>;
+}
+
+export default AppNav;
