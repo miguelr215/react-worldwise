32563e8baee1e9d3db769287fc4c42a0b08a235a
miguelr215 <drexelramos@gmail.com>
2025-04-17 16:04:28 -0400

    sidebar components added

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,8 +1,19 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './AppNav.module.css';
 
 function AppNav() {
-	return <nav className={styles.appNav}>App navigation</nav>;
+	return (
+		<nav className={styles.nav}>
+			<ul>
+				<li>
+					<NavLink to="cities">Cities</NavLink>
+				</li>
+				<li>
+					<NavLink to="countries">Countries</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
 }
 
 export default AppNav;
