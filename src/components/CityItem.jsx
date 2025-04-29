be6d251f4433f08e33a0e959a32ad8b4ca48ebf6
miguelr215 <drexelramos@gmail.com>
2025-04-29 06:43:59 -0400

    city component

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './CityItem.module.css';
 
@@ -10,16 +11,18 @@ const formatDate = (date) =>
 	}).format(new Date(date));
 
 function CityItem({ city }) {
-	const { cityName, emoji, flagPath, date } = city;
+	const { cityName, emoji, flagPath, date, id } = city;
 
 	return (
-		<li className={styles.cityItem}>
-			<span className={styles.emoji}>
-				<img src={flagPath} alt={`Flag of ${emoji}`} />
-			</span>
-			<h3 className={styles.name}>{cityName}</h3>
-			<time className={date}>{formatDate(date)}</time>
-			<button className={styles.deleteBtn}>&times;</button>
+		<li>
+			<Link className={styles.cityItem} to={`cities/${id}`}>
+				<span className={styles.emoji}>
+					<img src={flagPath} alt={`Flag of ${emoji}`} />
+				</span>
+				<h3 className={styles.name}>{cityName}</h3>
+				<time className={date}>{formatDate(date)}</time>
+				<button className={styles.deleteBtn}>&times;</button>
+			</Link>
 		</li>
 	);
 }
